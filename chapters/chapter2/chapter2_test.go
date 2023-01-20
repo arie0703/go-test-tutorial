9d18e87511903b2dab3c6844a9ae87908656bb2b
Kaisei <kaisei-arimura@dip-net.co.jp>
2023-01-20 16:33:06 +0900

    chapter2を解く

--- chapters/chapter2/chapter2_test.go
+++ chapters/chapter2/chapter2_test.go
@@ -3,6 +3,10 @@ package chapter2
 import (
 	"fmt"
 	"testing"
+
+	"github.com/dip-dev/go-test-tutorial/chapters/chapter2/communication"
+
+	"github.com/golang/mock/gomock"
 )
 
 func TestExecWithMock(t *testing.T) {
@@ -19,15 +23,16 @@ func TestExecWithMock(t *testing.T) {
 			fmt.Println(tc.want) // このfmtは一時的なエラー対応のため、テスト実装後に削除してください。
 
 			// FIXME: mock contoroller生成
-
+			ctrl := gomock.NewController(t)
 			// FIXME: mock設定
-
+			defer ctrl.Finish()
 			// FIXME: mockを構造体(Chapter2)に設定
-
+			mock := communication.NewMockInterfaceCommunication(ctrl)
 			// FIXME: exec関数を呼び出し
-
+			mock.EXPECT().Greeting().Return("Hello!")
 			// FIXME: 結果を検証
-
+			sample := New(mock)
+			sample.exec()
 		})
 	}
 }
